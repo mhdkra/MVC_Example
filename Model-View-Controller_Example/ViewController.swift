d01e42c76c8abc554417476c8d41d0e0460611a3
Tiara Mahardika <tiaramahardika@student.ub.ac.id>
2019-01-02 00:22:00 +0700

    Initial Commit

---
+++ Model-View-Controller_Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Model-View-Controller_Example
+//
+//  Created by Tiara Mahardika on 02/01/19.
+//  Copyright © 2019 Tiara Mahardika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
